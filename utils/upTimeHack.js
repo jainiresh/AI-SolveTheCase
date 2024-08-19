876299df8634d56cae88ea02656ddf42d6aeae9f
jai <nireshpandian19@gmail.com>
2024-08-20 01:38:17 +0530

    Up time hack added

---
+++ utils/upTimeHack.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const pingUrl = async() => {
+    try{
+        const response = await axios.get(pingUrl);
+        console.log(`Pinged url :${pingUrl} at ${new Date()} , Server is alive`);
+    }
+    catch(err){
+        console.error("Errored in pinging the url" , pingUrl);
+    }
+}
+module.exports = {pingUrl}
